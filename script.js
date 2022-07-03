3324f2b18504e6cc5d2541fae1528f57d6fd3a07
loubarb <louis.barbati@gmail.com>
2022-07-03 16:00:16 -0400

    Used eval function to convert equation string to number and display answer

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 const form = document.querySelector('#equation-form')
 const input = document.querySelector('#equation')
-const solveButton = document.querySelector('button')
 const results = document.querySelector('#results')
 
-form.addEventListener('keyup', (e) => {
-  const value = e.key
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const value = input.value
   const regex = /[0-9\+\-\*\/\^\)\(]/g
   const found = value.match(regex)
-  let equation = []
-  
   if (!found) return
-
-  if (found) {
-    equation.push(input.value)
-  }
-
-  const finalEquation = equation.join('')
-  console.log(finalEquation)
-
-  return finalEquation
+  const finalEquation = found.join('')
+  const answer = eval(finalEquation)
+  results.textContent = answer
   
-})
-
-solveButton.addEventListener('click', (e) => {
-  e.preventDefault()
-
 })
