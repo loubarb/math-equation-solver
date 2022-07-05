406e9fa4661612ace2ef45649457e936e9e1f693
loubarb <louis.barbati@gmail.com>
2022-07-04 23:13:33 -0400

    Added error div for invalid inputs

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const form = document.querySelector('#equation-form')
 const input = document.querySelector('#equation')
 const results = document.querySelector('#results')
+const errorDiv = document.querySelector('.error')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const value = input.value
-  const regex = /[0-9\+\-\*\/\^\)\(]/g
+  const regex = /[0-9\+\-\*\/\)\(]/g
   const found = value.match(regex)
-  if (!found) return
+  if (!found) {
+    errorDiv.textContent = 'Please enter a valid math equation.'
+  }
   const finalEquation = found.join('')
   const answer = eval(finalEquation)
   results.textContent = answer
+  errorDiv.textContent = ''
   
 })
+
